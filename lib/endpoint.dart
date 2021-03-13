6c51973c35681f123063d7a066c921cc3af0a6ad
Daniel Prado <dfe.prado@gmail.com>
2021-03-13 09:44:58 -0300

    Define a rota para requisição POST (padrão para recebimento de erros), a classe de erro ErrorPost e o formato do JSON de notificação de erro

--- lib/endpoint.dart
+++ lib/endpoint.dart
@@ -1,11 +1,45 @@
 import 'dart:io';
+import 'dart:convert';
+import 'errorPost.dart';
 
 class HeimdallEndpoint {
   HttpServer _server;
+
+  String _formatError(int code, String message, String type) {
+    final error = {
+      'code': code,
+      'message': message,
+      'type': type
+    };
+    return jsonEncode(error);
+  }
+
   void _onRequest(HttpRequest request) {
     print('A new ${request.method} request was received!');
-    request.response.statusCode = 200;
-    request.response.close();
+    if (request.method == 'GET') {
+      request.response.statusCode = 200;
+      request.response..
+        write('Hello World!')..
+        close();
+    }
+    else if (request.method == 'POST' && request.headers.contentType?.mimeType == 'application/json') {
+      utf8.decoder.bind(request).listen((String content) {
+        try {
+          var receivedError = ErrorPost.fromJson(content);
+          request.response
+            ..write('I received your error post. Thank you!')
+            ..close();
+
+          print(receivedError);
+        }
+        catch (e) {
+          request.response
+            ..statusCode = 400
+            ..write(_formatError(400, e.toString(), 'FormatException'))
+            ..close();
+        }
+      });
+    }
   }
 
   void start() async {
--- lib/errorPost.dart
+++ lib/errorPost.dart
@@ -1,3 +1,36 @@
+import 'dart:core';
+import 'dart:convert';
+
+const TIMESTAMP_KEY = 'timestamp';
+const TYPE_KEY = 'type';
+const SCOPE_KEY = 'scope';
+const MESSAGE_KEY = 'message';
+const PROGRAM_KEY = 'program';
+
 class ErrorPost {
-  
+  DateTime _timestamp;
+  String _type;
+  String _scope;
+  String _message;
+  String _program;
+
+  ErrorPost.fromJson(String jsonString) {
+    var json = jsonDecode(jsonString) as Map;
+
+    if (!json.containsKey(TIMESTAMP_KEY) || !json.containsKey(TYPE_KEY) || !json.containsKey(SCOPE_KEY) || !json.containsKey(MESSAGE_KEY) || !json.containsKey(PROGRAM_KEY)) {
+      throw FormatException('Wrong body format');
+    }
+
+    _timestamp = DateTime.parse(json[TIMESTAMP_KEY]);
+    _type = json[TYPE_KEY];
+    _scope = json[SCOPE_KEY];
+    _message = json[MESSAGE_KEY];
+    _program = json[PROGRAM_KEY];
+  } 
+
+  DateTime get timestamp => _timestamp;
+  String get type => _type;
+  String get scope => _scope;
+  String get message => _message;
+  String get program => _program;
 }
